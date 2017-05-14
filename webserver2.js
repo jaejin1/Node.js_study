df1a1843a193f6883e44352f9bd5738e9e5c26e0
jaejin1 <opiximeo@gmail.com>
2017-05-14 16:08:29 +0900

    basic

---
+++ webserver2.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+var server2 = http.createServer(function(req,res){  //요청, 응답 객체들
+  //어떻게 응답할것인가.
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server2.listen(port , hostname, function(){
+  console.log(`Server running at http://${hostname}:${port}/`);
+  //서버가 listening에 성공했을때 출력되는 코드
+});
