4ccff493769992ea1858a04fd9d74dbfa306b5cf
jaejin1 <opiximeo@gmail.com>
2017-05-20 23:51:39 +0900

    template엔진 사용하기

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express(); //익스프레스를 만든사람이 내가 만든것은 이렇게 해야해요
 
-app.use(express.static('public'));  //정적인 파일이 위치하는 곳을 지정한다.
+app.set('view engine','jade');
+app.set('views','./views');   //jade 파일은 views 안에다 넣어야한다.  기본값으로 views를 가지고 있음.
 
+app.use(express.static('public'));  //정적인 파일이 위치하는 곳을 지정한다.
+app.get('/template', function(req,res){
+	res.render('temp');  //템플릿 파일을 읽어오는ㄱ누나
+})
 app.get('/', function(req, res){
 	  //send()를 응답할것이다.
 	res.send('네이버 접속하기  <a href="https://www.naver.com">접속</a>');
