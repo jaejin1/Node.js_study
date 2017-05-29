15b9d739a5bfad6d7a7714f2fb069ab3ae704727
jaejin1 <opiximeo@gmail.com>
2017-05-29 21:59:48 +0900

    웹 어플

--- app_file.js
+++ app_file.js
@@ -11,25 +11,24 @@ app.set('view engine','pug');
 app.locals.pretty = true ;
 
 app.get('/topic/new', function(req, res){
-  res.render('new');
+  fs.readdir('data', function(err,files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('new', {topics:files});
+  });
 })
-app.get('/topic', function(req,res){
-  fs.readdir('data', function(err,files){
-    if(err){
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-    res.render('view',{topics:files});
-  })
-});
-app.get('/topic/:id', function(req,res){
-  var id = req.params.id;
 
+app.get(['/topic', '/topic/:id'], function(req,res){
   fs.readdir('data', function(err,files){
     if(err){
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
+    var id = req.params.id;
+    if(id){
+    // id 값이 있을 때
     fs.readFile('data/'+id, 'utf8', function(err, data){
       if(err){
         console.log(err);
@@ -37,8 +36,29 @@ app.get('/topic/:id', function(req,res){
       }
       res.render('view',{title:id, topics:files, description: data});
     })
+  } else {
+    // if 값이 없을때
+    res.render('view',{topics:files, title:'Welcome', description:'Hello , javascript for Server'});
+  }
   })
-})
+});
+// app.get('/topic/:id', function(req,res){
+//   var id = req.params.id;
+//
+//   fs.readdir('data', function(err,files){
+//     if(err){
+//       console.log(err);
+//       res.status(500).send('Internal Server Error');
+//     }
+//     fs.readFile('data/'+id, 'utf8', function(err, data){
+//       if(err){
+//         console.log(err);
+//         res.status(500).send('Internal Server Error');
+//       }
+//       res.render('view',{title:id, topics:files, description: data});
+//     })
+//   })
+// })
 app.post('/topic', function(req, res){
   var title = req.body.title;
   var description = req.body.description;
@@ -47,7 +67,7 @@ app.post('/topic', function(req, res){
       console.log(err);
       res.status(500).send('Internal Server Error');   //한번 Send되면 다른 Send는 실행 X
     }
-    res.send('Successs! ');
+    res.redirect('/topic/'+title);
   });
 })
 
